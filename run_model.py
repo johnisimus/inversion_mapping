d6e4c46216edb0801875ee72607006f99a7dbc1c
John <johnisimus@gmail.com>
2018-06-25 22:01:47 +0100

    script for running the full model

---
+++ run_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 15:14:59 2018
+
+@author: jstockdale
+"""
+
+from model_1 import Model
+
+ema_path = 'mngu0_s1_ema_norm_1.0.1/'
+lsf_path = 'mngu0_s1_lsf_norm_1.0.1'
+training_path = 'trainfiles.txt'
+
+model = Model()
+model.load_ema_data(ema_path)
+model.load_lsf_data(lsf_path)
+model.add_training_files(training_path)
+model.define_model(128, 41)
+model.train_model(num_epochs=100)
+
+model_json = model.model.to_json()
+with open('model.json', 'w') as json_file:
+    json_file.write(model_json)
+
+model.model.save_weights('model.h5')
+
